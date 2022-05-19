776f72c1996f5011997626b0bc1c01c9eb672093
an19179 <105824846+an19179@users.noreply.github.com>
2022-05-20 09:41:47 +1200

    Idle python

---
+++ idle2.py
@@ -0,0 +1,5 @@
+username= input("Welcome, please enter your name")
+print ("Hello" + username)
+
+userage = input("Hello",+ username,"how old are you?")
+print (username, "is " + username, "years old") 
